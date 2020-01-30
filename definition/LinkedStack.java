7db25badede03936d31445927cc5f6c548a3be5e
saurabh <ssroyal>
2020-01-30 11:05:34 +0530

    Add LinkedStack class

---
+++ definition/LinkedStack.java
@@ -0,0 +1,27 @@
+package definition;
+
+import Adt.StackADT;
+
+public class LinkedStack<E> implements StackADT {
+
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public void push(Object data) {
+
+    }
+}
